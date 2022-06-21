1cf20e67121671000daad091e212417d2be4a856
raheemelassadi <raheemelassadi1@gmail.com>
2022-06-21 15:54:43 -0700

    file initialization

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+  };
