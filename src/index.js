d4e1ff2caf1c4cd7b6f0d6f73207c0aafa50e146
raheemelassadi <raheemelassadi1@gmail.com>
2022-07-01 01:50:54 -0700

    introduced logic for switching between projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.scss"
 import { sumbitProject } from "./modules/dom"
-import closeBtn from "./modules/utils"
+import { closeBtn } from "./modules/utils"
 
 sumbitProject()
 closeBtn()
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,11 +2,11 @@ import { Folder, btnLogic } from "./utils";
 
 const sumbitProject = () => {
     const name = document.querySelector("#name")
-    const form = document.querySelector('#myModal')
     const submitBtn = document.querySelector('#modalSubmit')
 
     submitBtn.addEventListener('click', function(){
         const newProject = new Folder()
+        newProject.pageCreate()
         name.value = ""
     })
 }
